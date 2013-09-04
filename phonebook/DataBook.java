abf3d3f8e42a4af112e67cf04af2736821acbde3
airkim123 <airkim123@ajantech.com>
2013-09-04 11:28:42 +0900

    method

--- phonebook/DataBook.java
+++ phonebook/DataBook.java
@@ -1,4 +1,3 @@
-package phonebook;
 
 
 class DataBook
--- phonebook/PhoneBookMain.java
+++ phonebook/PhoneBookMain.java
@@ -1,97 +1,132 @@
-package phonebook;
 
 
 import java.io.*;
 
 class PhoneBook
 {
-	public static void main(String[] args) throws IOException
-	{
-		String str = "";
-		int i = -1;
-		DataBook data = new DataBook();
-		MyConsole con = new MyConsole();
-		con.clearConsole();
-	while(true)
+	private DataBook databook;
+    private	String strscanConsoleInput;
+
+	private void showMainMenu()
 	{
 		System.out.println("==========================================");
-		System.out.println("   1. input						");
-		System.out.println("   2. search					");
-		System.out.println("   3. show list					");
-		System.out.println("   4. exit						");
+		System.out.println("   PhoneBook");
+		System.out.println("   1. input");
+		System.out.println("   2. search");
+		System.out.println("   3. show list");
+		System.out.println("   4. exit");
 		System.out.println("==========================================");
+	}
 
-		KeyboardInput in = new KeyboardInput();
-		try {
-		str = in.scanInput();
-		i = Integer.parseInt(str);
-		} catch (NumberFormatException nfe) {
-			System.err.println(nfe);
-			System.exit(1);
-		}
-		
+	private void inputData()
+	{	
+		String strinputData = "";
+		databook = new DataBook();
 
-		switch(i)
+		System.out.println("input name");	
+		strinputData = scanConsoleInput();
+		databook.setFields(databook.getKey(),"NAME", strinputData);
+		System.out.println("input number");	
+		strinputData = scanConsoleInput();
+		databook.setFields(databook.getKey(),"NUMBER", strinputData);
+		System.out.println("input memo");	
+		strinputData = scanConsoleInput();
+		databook.setFields(databook.getKey(),"MEMO", strinputData);
+				
+		if( 10000 == databook.setKey())
+		{
+			System.out.println("No Space to fill out");	
+		}					
+	}
+
+	private void searchData()
+	{
+		String strsearchData = "";
+		int caseI;
+		System.out.println("--------------------------------");
+		System.out.println(" search for 					");
+		System.out.println("   1. name						");
+		System.out.println("   2. number					");
+		System.out.println("   3. memo   					");
+		System.out.println("--------------------------------");
+
+		strsearchData = scanConsoleInput();
+		caseI = Integer.parseInt(strsearchData);
+
+		switch(caseI)
 		{
 			case 1:
-				System.out.println("input name");	
-				data.setFields(data.getKey(),"NAME", in.scanInput());
-				System.out.println("input number");	
-				data.setFields(data.getKey(),"NUMBER", in.scanInput());
-				System.out.println("input memo");	
-				data.setFields(data.getKey(),"MEMO", in.scanInput());
-				
-				if( 10000 == data.setKey())
-				{
-					System.out.println("No Space to fill out");	
-				}					
-
+				System.out.println("input name for search");
+				strsearchData = scanConsoleInput();
+				databook.getFields(databook.lookupFields(strsearchData));
 			break;
-
 			case 2:
-				con.clearConsole();
-				System.out.println("--------------------------------");
-				System.out.println("   1. name						");
-				System.out.println("   2. number					");
-				System.out.println("   3. memo   					");
-				System.out.println("--------------------------------");
-				try {
-				str = in.scanInput();
-				i = Integer.parseInt(str);
-				} catch (NumberFormatException nfe) {
-					System.err.println(nfe);
-					System.exit(1);
-				}
-				switch(i)
-				{
-					case 1:
-						System.out.println("input name for search");
-						data.getFields(data.lookupFields(in.scanInput()));
-					break;
-					case 2:
-					break;
-					case 3:
-					break;
-					default:
-					break;
-				}
-				
 			break;
-
 			case 3:
-				for(int cnt=0; cnt < data.getKey(); cnt++)
-					data.getFields(cnt);
 			break;
-
-			case 4:
-				System.exit(1);
-			break;
-
 			default:
 			break;
 		}
-
 	}
+
+	private void showDataList()
+	{
+		for(int cnt=0; cnt < databook.getKey(); cnt++)
+			databook.getFields(cnt);
+	}
+
+	public String scanConsoleInput()
+	{
+		KeyboardInput in = new KeyboardInput();
+		try{
+		strscanConsoleInput = in.scanInput();
+		}
+		catch(IOException e)
+		{
+		}
+
+		return strscanConsoleInput;
+	}
+
+
+	public static void main(String[] args) throws IOException
+	{
+		String strmain = "";
+		int strmainI;
+		MyConsole myconsole = new MyConsole();
+		myconsole.clearConsole();
+
+		PhoneBook mainbook = new PhoneBook();
+
+		while(true)
+		{
+			mainbook.showMainMenu();
+			strmain = mainbook.scanConsoleInput();
+			strmainI = Integer.parseInt(strmain);
+	
+			switch(strmainI)
+			{
+				case 1:
+					mainbook.inputData();
+				break;
+	
+				case 2:
+					mainbook.searchData();
+				break;
+	
+				case 3:
+					mainbook.showDataList();
+				break;
+	
+				case 4:
+					System.exit(1);
+				break;
+	
+				default:
+				break;
+			}
+	
+		}
 	}
 
 }
@@ -105,26 +140,3 @@ class MyConsole
 	public native static void clearConsole();
 		
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
